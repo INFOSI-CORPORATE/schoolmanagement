2f9a36266ee9fb14a74d015bde499ea26d75b355
eliudefrancisco14 <eliudepauloquintasf@example.com>
2023-06-29 12:41:04 +0100

    Resolvendo problema de storage

--- app/Http/Controllers/Admin/PDFController.php
+++ app/Http/Controllers/Admin/PDFController.php
@@ -195,8 +195,10 @@ class PDFController extends Controller
 
     public function transportDocumentation($id)
     {
-        $transport = transport::find($id);        
-        $path = storage_path('app/' . $transport->documentation);
+        $transport = transport::find($id);     
+        $path = storage_path('app/'.$transport->documentation);   
+
+
         if(!file_exists($path)){
             return redirect()->back()->with('documentation_not_exist','1');
         }
